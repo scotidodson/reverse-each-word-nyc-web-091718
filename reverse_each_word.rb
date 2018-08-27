8e397d42408fdce0934cd356ee04bd46c8a73953
Scoti Dodson <scotidodson@gmail.com>
2018-08-27 21:23:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  array.collect do |word|
+  reversed_array = array.collect do |word|
     word.reverse 
   end 
-  array.join(" ")
+  reversed_array.join(" ")
 end 
   
